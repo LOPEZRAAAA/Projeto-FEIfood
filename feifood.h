eb9e524d0fb8d7445a8ebbec0be6b3b1a1e606e3
LOPEZRAAAA <rafaelcorreialopez@gmail.com>
2025-10-31 19:40:00 -0300

    Projeto I

---
+++ feifood.h
@@ -0,0 +1,69 @@
+//
+// Created by feisprlopez on 31/10/2025.
+//
+
+#ifndef CLIONPROJECTS_FEIFOOD_H
+#define CLIONPROJECTS_FEIFOOD_H
+
+#endif //CLIONPROJECTS_FEIFOOD_H
+
+void cadastrar_pedido() {
+
+}
+int criar_pedido() {
+
+}
+int editar_pedido() {
+
+}
+int excluir_pedido() {
+
+}
+int alimentos_pedidos() {
+
+}
+int avaliar_pedido() {
+
+}
+int cadastrar_usuario() {
+
+}
+int login_usuario() {
+
+}
+int buscar_alimento() {
+
+}
+int listar_infos() {
+
+}
+int login_admin() {
+
+}
+int cadastrar_alimento() {
+
+}
+int excluir_alimento() {
+
+}
+int cadastrar_lojas() {
+
+}
+int consultar_usuarios() {
+
+}
+int top_alimentos() {
+
+}
+int top_odiados() {
+
+}
+int tot_usuarios() {
+
+}
+int tot_estabelecimentos() {
+
+}
+int total_alimentos() {
+
+}
---
+++ main.c
@@ -0,0 +1,11 @@
+//
+// Created by feisprlopez on 31/10/2025.
+//
+#include "feifood.h"
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+printf("Bem Vindo ao FEIfood!\n");
+
+}
